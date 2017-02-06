7d9fbecf13b657918fa2aa9e1c0bdda2631dd9c9
serska15 <serska15@b048-09.crio.dauphine.fr>
2017-02-06 13:31:17 +0100

    ajout des exercices 4.3 et 4.7

---
+++ exo4_3/src/exo4_3/Ex4_3.java
@@ -0,0 +1,38 @@
+package exo4_3;
+
+import java.util.Random ;
+import java.util.Scanner;
+import java.lang.IllegalArgumentException ;
+public class Ex4_3 {
+	private static Scanner sc;
+
+	public static void main (String args []) throws IllegalArgumentException{
+		System.out.println("Rentrez un nombre") ;
+		sc = new Scanner(System.in);
+		int nbr =  sc.nextInt();
+		if(nbr < 2 || nbr>12){
+			throw new IllegalArgumentException();
+		}
+		System.out.println("Votre nombre est tombé au bout de "+ sommeDe(nbr)+" lancés");
+	}
+	
+	public static int sommeDe(int nbr){
+		int compteur = 0;
+
+		Random generator = new Random();
+		int de1 = generator.nextInt(5) +1 ;
+		int de2 = generator.nextInt(5) +1 ;
+
+		int sommede = de1+de2;
+		
+		while(sommede != nbr){
+			
+			compteur++;
+			de1 = generator.nextInt(5) +1 ;
+			de2 = generator.nextInt(5) +1 ;
+			sommede = de1+de2;
+			
+		}
+			return compteur ;
+	}
+}
---
+++ exo4_7/src/exo47/Quiz.java
@@ -0,0 +1,45 @@
+package exo47;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Quiz {
+	public static int oper1[]=new int[10];
+	public static int oper2[]=new int[10];
+	public static int resultat[]=new int[10];
+	
+	public static void creerQuestionnaire(){
+		int i;
+		Random rand = new Random();
+		for(i=0;i<10;i++){
+			oper1[i]=rand.nextInt(51);
+			oper2[i]=rand.nextInt(51);
+		}
+	}
+	public static void administrerQuiz(){
+		int i;
+		Scanner sc;
+		for(i=0;i<10;i++){
+			System.out.print("question N°"+ (i+1) + ": "+ oper1[i]+ " + " + oper2[i]+ " = ?");
+			sc = new Scanner(System.in);
+		    resultat[i] = sc.nextInt();
+		}
+	}
+	public static void evaluerQuiz(){
+		int i;
+		System.out.println("les bonnes réponses sont:");
+		for(i=0;i<10;i++){
+		    if(resultat[i]==(oper1[i]+oper2[i])){
+		    	System.out.println("question N°"+ (i+1) + ": "+ oper1[i]+ " + " + oper2[i]+ " = "+(oper1[i]+oper2[i])+". Votre réponse est correcte.");
+			score+=10;		    
+		}else{
+		    	System.out.println("question N°"+ (i+1) + ": "+ oper1[i]+ " + " + oper2[i]+ " = "+(oper1[i]+oper2[i])+". Vous avez trouvez "+resultat[i]+", votre réponse n'est pas correcte.");
+		    }
+		}
+		System.out.println("votre score est de "+score+" points");
+	}
+	public static void main(String[] args) {
+		creerQuestionnaire();
+		administrerQuiz();
+		evaluerQuiz();
+	}
+}
