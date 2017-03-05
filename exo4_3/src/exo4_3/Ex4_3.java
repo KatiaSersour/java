13fef208cb229fa62bef0a231a5372153ef46a16
KatiaSersour <ser-katia@hotmail.fr>
2017-03-05 12:45:00 +0100

    Ex4.3

--- exo4_3/src/exo4_3/Ex4_3.java
+++ exo4_3/src/exo4_3/Ex4_3.java
@@ -3,6 +3,12 @@ package exo4_3;
 import java.util.Random ;
 import java.util.Scanner;
 import java.lang.IllegalArgumentException ;
+
+/**
+ * 
+ * @author KATIA
+ *
+ */
 public class Ex4_3 {
 	private static Scanner sc;
 
@@ -13,9 +19,14 @@ public class Ex4_3 {
 		if(nbr < 2 || nbr>12){
 			throw new IllegalArgumentException();
 		}
-		System.out.println("Votre nombre est tombé au bout de "+ sommeDe(nbr)+" lancés");
+		System.out.println("Votre nombre est tombÃ© au bout de "+ sommeDe(nbr)+" lancÃ©s");
 	}
 	
+	/**
+	  * 
+	  * @param nbr
+	  * @return the number of times you have to roll the dice
+	  */
 	public static int sommeDe(int nbr){
 		int compteur = 0;
 
